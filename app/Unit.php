f3b69b7d91cc3ce1e146183c6d79296f8f633feb
witekf <tauinstalacje@gmail.com>
2018-12-08 16:48:09 +0100

    dodanie soft delete dla units

--- app/Unit.php
+++ app/Unit.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Unit extends Model
+
 {
+    use SoftDeletes;
+
+
     protected $fillable = ['code', 'name'];
+    protected $dates = ['deleted_at'];
 }
--- database/migrations/2018_12_08_134104_create_units_table.php
+++ database/migrations/2018_12_08_134104_create_units_table.php
@@ -17,6 +17,7 @@ class CreateUnitsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('code');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
